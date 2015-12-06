957676cadb84d88f7dfb1256e0510f41d7dfa580
ganzw <ganzw@guahao.com>
2015-12-06 19:24:14 +0800

    Fix first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+var Filter = require('broccoli-filter');
+var RSVP = require('rsvp');
+var coffeeScript = require('coffee-script');
+
+
+
+function CoffeeScriptFilter(inputTree, options) {
+    if (!(this instanceof CoffeeScriptFilter)) {
+        return new CoffeeScriptFilter(inputTree, options)
+    }
+    Filter.call(this, inputTree, options);
+    this.options = options || {wrapper: false,sourceMap: false,header: ''};
+}
+
+CoffeeScriptFilter.prototype = Object.create(Filter.prototype)
+CoffeeScriptFilter.prototype.constructor = CoffeeScriptFilter
+
+CoffeeScriptFilter.prototype.extensions = ['coffee', 'cf']
+CoffeeScriptFilter.prototype.targetExtension = 'js'
+
+var coffeeScriptOptions = {
+    bare: this.options.wrapper,
+    sourceMap: this.options.sourceMap,
+    header: this.options.header
+}
+
+
+CoffeeScriptFilter.prototype.processString = function(string, srcFile) {
+    return new RSVP.Promise(function(resolve, reject) {
+        try {
+            resolve(coffeeScript.compile(string, coffeeScriptOptions));
+        } catch (err) {
+            reject(err);
+        }
+    }.bind(this));
+}
+module.exports = CoffeeScriptFilter
---
+++ test.js
@@ -0,0 +1,30 @@
+'use strict'
+/**
+ * test compile coffeescript
+ * @type {[type]}
+ */
+var coffeescript = require('./');
+var fixture = require('broccoli-fixture');
+var chai = require('chai'),
+	expect = chai.expect;
+var chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+
+describe('broccoli-coffee', function() {
+	var options;
+	before(function() {
+		options = {
+			wrapper: false,
+			sourceMap: false,
+			header: ''
+		};
+	});
+	it('compiles .coffee or .cf files', function() {
+		var inputNode = new fixture.Node({
+			'test.coffee': 'console.log "Hello world"'
+		})
+		return expect(fixture.build(new BroccoliCoffee(inputNode, options))).to.eventually.deep.equal({
+			'test.js': '(function() {\n  console.log(\"Hello world\");\n\n}).call(this);\n'
+		})
+	})
+})
